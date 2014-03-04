b3b69aa8a4fb7a7c838f96a43ec88b39eb570a38
Amol Bhangdiya <abhangdiya@yahoo.co.in>
2014-03-04 13:26:10 +0530

    Update raft.go

--- raft.go
+++ raft.go
@@ -24,22 +24,30 @@ func main() {
 
 	CurrLeader := make(chan int, 10)
 	raft1 := new(raft.RaftImpl)
-	raft1.Configure(5556, configPath, clust.Random(5, 30), 1, CurrLeader, 0.0, 0)
+	exit1 := make(chan int, 1)
+	raft1.Configure(5556, configPath, clust.Random(5, 30), 1, CurrLeader, 0.0, 0, exit1)
 
 	raft2 := new(raft.RaftImpl)
-	raft2.Configure(5557, configPath, clust.Random(5, 30), 1, CurrLeader, 0.0, 0)
+	exit2 := make(chan int, 1)
+	raft2.Configure(5557, configPath, clust.Random(5, 30), 1, CurrLeader, 0.0, 0, exit2)
 
 	raft3 := new(raft.RaftImpl)
-	raft3.Configure(5558, configPath, clust.Random(5, 30), 1, CurrLeader, 0.0, 0)
+	exit3 := make(chan int, 1)
+	raft3.Configure(5558, configPath, clust.Random(5, 30), 1, CurrLeader, 0.0, 0, exit3)
 
 	raft4 := new(raft.RaftImpl)
-	raft4.Configure(5559, configPath, clust.Random(5, 30), 1, CurrLeader, 0.0, 0)
+	exit4 := make(chan int, 1)
+	raft4.Configure(5559, configPath, clust.Random(5, 30), 1, CurrLeader, 0.0, 0, exit4)
 
 L:
 	for {
 		select {
 		case leader := <-CurrLeader:
 			TRACE.Println("Server id ", leader, " is the leader")
+			exit1 <- 1
+			exit2 <- 1
+			exit3 <- 1
+			exit4 <- 1
 			break L
 		}
 	}
